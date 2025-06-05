c5adce68fceeae57b1af44707dea437c946bbc4e
Sean Hatton <sean.n.hatton@gmail.com>
2025-06-05 09:50:21 -0700

    FreeSurfer annotation

--- scripts/run_freesurfer_annotation.sh
+++ scripts/run_freesurfer_annotation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-for sub in sub-*;do
-    printf "************* Processing ${sub} *************\n"
+for sub in sub-001 sub-002 sub-003 sub-004 sub-005 sub-006 sub-007 sub-008 sub-009 sub-010;do
+ printf "************* Processing ${sub} *************\n"
     segstats2nidm -add_de -s /media/sehatton/ds004604/derivatives/freesurfer/${sub} -o /media/sehatton/ds004604/derivatives/freesurfer/${sub} -subjid ${sub} -forcenidm
 done
