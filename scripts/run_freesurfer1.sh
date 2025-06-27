8ee6d656172b8c5f1a2d711544d6a799862c0e07
Sean Hatton <sean.n.hatton@gmail.com>
2025-06-27 09:30:12 -0700

    Git fix

--- scripts/run_freesurfer1.sh
+++ scripts/run_freesurfer1.sh
@@ -3,10 +3,10 @@
 for sub in sub-001 sub-002 sub-003 sub-004 sub-005 sub-006 sub-007 sub-008 sub-009 sub-010;do
     printf "************* Processing ${sub} *************\n"
     docker run -it --rm \
-    -v /media/sehatton/ds004604_fs/license/license.txt:/usr/local/freesurfer/.license \
+    -v /media/sehatton/FAIR_neuroimaging_with_NIDM/license/license.txt:/usr/local/freesurfer/.license \
     -e FS_LICENSE='/usr/local/freesurfer/.license' \
-    -v /media/sehatton/ds004604_fs/derivatives/freesurfer:/home/freesurfer/subjects \
-    -v /media/sehatton/ds004604_fs/:/data \
+    -v /media/sehatton/FAIR_neuroimaging_with_NIDM/derivatives/freesurfer:/home/freesurfer/subjects \
+    -v /media/sehatton/FAIR_neuroimaging_with_NIDM/:/data \
     -e SUBJECTS_DIR='/home/freesurfer/subjects' \
     freesurfer/freesurfer:7.4.1 \
     recon-all -all -s ${sub} -i /data/${sub}/anat/${sub}_T1w.nii.gz
--- scripts/run_freesurfer5.sh
+++ scripts/run_freesurfer5.sh
@@ -3,10 +3,10 @@
 for sub in sub-041 sub-042 sub-043 sub-044 sub-045 sub-046 sub-047 sub-048 sub-049 sub-050;do
     printf "************* Processing ${sub} *************\n"
     docker run -it --rm \
-    -v /media/sehatton/ds004604_fs/license/license.txt:/usr/local/freesurfer/.license \
+    -v /media/sehatton/FAIR_neuroimaging_with_NIDM/license/license.txt:/usr/local/freesurfer/.license \
     -e FS_LICENSE='/usr/local/freesurfer/.license' \
-    -v /media/sehatton/ds004604_fs/derivatives/freesurfer:/home/freesurfer/subjects \
-    -v /media/sehatton/ds004604_fs/:/data \
+    -v /media/sehatton/FAIR_neuroimaging_with_NIDM/derivatives/freesurfer:/home/freesurfer/subjects \
+    -v /media/sehatton/FAIR_neuroimaging_with_NIDM/:/data \
     -e SUBJECTS_DIR='/home/freesurfer/subjects' \
     freesurfer/freesurfer:7.4.1 \
     recon-all -all -s ${sub} -i /data/${sub}/anat/${sub}_T1w.nii.gz
