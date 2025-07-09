724546f12e45923b700fcbcab81c4c560935ab6c
Sean Hatton <sean.n.hatton@gmail.com>
2025-07-09 10:27:31 -0700

    Added FS files and annotated BIDS

--- scripts/run_freesurfer_annotation.sh
+++ scripts/run_freesurfer_annotation.sh
@@ -3,5 +3,5 @@
 
 for sub in sub-*;do
     printf "************* Processing ${sub} *************\n"
-    segstats2nidm -add_de -s /media/sehatton/ds004604/derivatives/freesurfer/${sub} -o /media/sehatton/ds004604/derivatives/freesurfer/${sub} -subjid ${sub} -forcenidm
+    segstats2nidm -add_de -s /media/sehatton/FAIR_neuroimaging/derivatives/freesurfer/${sub} -o /media/sehatton/FAIR_neuroimaging/derivatives/freesurfer/${sub} -subjid ${sub} -forcenidm
 done
